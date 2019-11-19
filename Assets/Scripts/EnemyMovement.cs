8ec1ad9aed3120378b96d84289c51780b9fbe57d
Keegan Frederick <keegandfrederick@gmail.com>
2019-11-18 22:50:47 -0800

    Added initial enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float enemyVelocity;
+    public float bounce;
+
+    private float xDir;
+    private float yDir;
+    private float directionAngle;
+    private Vector2 movementDirection;
+    private Vector2 movementSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeMovementDirection();
+    }
+
+    void changeMovementDirection()
+    {
+        xDir = Random.Range(-1.0f, 1.0f);
+        yDir = Random.Range(-1.0f, 1.0f);
+        movementDirection = new Vector2(xDir, yDir).normalized;
+        directionAngle = Vector2.Angle(xDir, yDir);
+        transform.Rotate(movementDirection);
+        movementSpeed = movementDirection * enemyVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = new Vector2(transform.position.x + (movementSpeed.x * Time.deltaTime), 
+            transform.position.y + (Time.deltaTime * movementSpeed.y));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.name);
+        transform.position -= bounce * (Vector3)movementDirection;
+        changeMovementDirection();
+        
+    }
+}
