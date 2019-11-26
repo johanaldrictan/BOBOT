53ea1c93579399c48c9adc8088d3617be949cc6a
Johan Tan <johan.tan21@gmail.com>
2019-11-26 13:08:36 -0800

    Added Charge Dash and Bouncy walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float acceleration;
-    public float dashSpeed;
     public Vector2 maxVelocity;
     public float dashCooldown = .5f;
     Vector2 lastMoveDir;
 
+    public float maxDash = 500;
+    public float chargeRate = 100;
+    public float initialDash = 100;
+    float dashSpeed = 0;
+    bool charging = false;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        dashSpeed = initialDash;
     }
     // Start is called before the first frame update
     void Start()
@@ -25,7 +31,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     private void FixedUpdate()
     {
@@ -37,15 +43,28 @@ public class PlayerController : MonoBehaviour
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         if (input.magnitude > 1)
             input.Normalize();
-        lastMoveDir = input;
+        if(input.magnitude != 0)
+            lastMoveDir = input;
         if (Mathf.Abs(rb.velocity.x) < Mathf.Abs(maxVelocity.x) && Mathf.Abs(rb.velocity.y) < Mathf.Abs(maxVelocity.y))
             rb.velocity += input * acceleration;
     }
     private void HandleDash()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(lastMoveDir * dashSpeed);
+            //Debug.Log("Charging");
+            charging = true;
+        }
+        //not doing time.deltatime here should be fine
+        if (charging && dashSpeed < maxDash)
+            dashSpeed += (chargeRate);
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //Debug.Log("Firing");
+            Debug.Log("Dash Speed: " + dashSpeed);
+            rb.AddForce(lastMoveDir * dashSpeed, ForceMode2D.Impulse);
+            dashSpeed = initialDash;
+            charging = false;
         }
     }
 }
