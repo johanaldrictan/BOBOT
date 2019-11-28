880ba207704826bdf75e4b29a166fab608b1a39c
waffle112 <justinlin920@gmail.com>
2019-11-27 22:49:48 -0800

    Merge branch 'master' of https://github.com/johanaldrictan/BOBOT

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Switch to 640 x 480 full-screen
+        //Screen.SetResolution(800, 600, false);
+        offset = transform.position - player.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player!=null)
+            transform.position = player.transform.position + offset;
+    }
+}
