144e2790a72d6353f9d12775c395ae3273be83f8
waffle112 <justinlin920@gmail.com>
2019-11-28 23:55:07 -0800

    added basic menu without the other scenes tho

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResumeGame();
+    }
+    public static bool GameIsPaused = false;
+    public GameObject pui;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pui.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void PauseGame()
+    {
+        pui.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMainMenu()
+    {
+        Debug.Log("for Main menu ");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+
+}
